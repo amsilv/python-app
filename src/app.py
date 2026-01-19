54c4aa9e2b083fb3fc4acb20cb924a79b7c3458d
Alexandre Silva <alexandreamsilv@gmail.com>
2026-01-19 16:07:39 -0300

    Adds python-app aplication with k8s deployments

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details', methods=['GET'])
+def details():
+    return jsonify({
+        'time': datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'message': 'Luciana Deu certo!! uhuuuu'
+    })
+
+@app.route('/api/v1/healthz', methods=['GET'])
+def healthz():
+    return jsonify({'status': 'up and running'}), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
