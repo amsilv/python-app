d05d62b303724ff18d893a8e6481cfb5a6bd5876
Alexandre Silva <alexandreamsilv@gmail.com>
2026-01-20 10:52:45 -0300

    Adds cd installation

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'time': datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'you are doing great, agora tem que funcionar, é muita coisa rs!!!:)'
+        'message': 'you are doing great, agora tem que funcionar, é muita coisa rs, mas coisa rs!!!:)'
     })
 
 @app.route('/api/v1/healthz', methods=['GET'])
