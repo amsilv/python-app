990a92f164b8a977320ef4658cfdc9627fb956c6
Alexandre Silva <alexandreamsilv@gmail.com>
2026-01-20 08:51:52 -0300

    nova imagem

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'time': datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'you are doing great, agora tem que funcionar!!!:)'
+        'message': 'you are doing great, agora tem que funcionar, nova app!!!:)'
     })
 
 @app.route('/api/v1/healthz', methods=['GET'])
